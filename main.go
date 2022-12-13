cef6dc87c6e64485694a44236607e179a5b81861
Dhaval Shah <dhavalshah@gofynd.com>
2022-12-13 12:04:21 +0530

    Show how to create time and how to format it

--- main.go
+++ main.go
@@ -1,13 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	var result = 0
-	add(1, 3, &result)
-	fmt.Println("Result is ", result)
+	printTimeUsingFormat()
 }
 
-func add(num1 int, num2 int, result *int) {
-	*result = num1 + num2
+func printTimeUsingFormat() {
+	datetime := time.Now()
+	fmt.Println("Year :", datetime.Format("2006/06"))
+	fmt.Println("Month :", datetime.Format("01/Jan/January"))
+	fmt.Println("Day of month :", datetime.Format("02/Mon/Monday"))
+	fmt.Println("Hour 24 :", datetime.Format("15"))
+	fmt.Println("Hour 12 :", datetime.Format("03 PM/pm"))
+	fmt.Println("Minutes :", datetime.Format("04"))
+	fmt.Println("Seconds :", datetime.Format("05"))
+	fmt.Println("Timezone :", datetime.Format("-07/-0700/MST"))
+}
+
+func printTimeUsingDate() {
+	datetime := time.Date(2022, 12, 13, 8, 30, 0, 0, time.Local)
+	printTimeManually(datetime)
+}
+
+func printTimeNow() {
+	datetime := time.Now()
+	printTimeManually(datetime)
+}
+
+func printTimeManually(datetime time.Time) {
+	fmt.Println(datetime)
+	fmt.Printf("Year: %d, Month: %d, Day: %d\n", datetime.Year(), datetime.Month(), datetime.Day())
+	fmt.Printf("Hour: %d, Minute: %d, Second: %d\n", datetime.Hour(), datetime.Minute(), datetime.Second())
+	fmt.Printf("Weekday: %v\n", datetime.Weekday())
 }
