53ed1bfbd39c995fce9c89ad7dee415a7337edb1
Dhaval Shah <dhavalshah@gofynd.com>
2022-12-16 11:19:18 +0530

    Add basic type conversions

--- main.go
+++ main.go
@@ -1,13 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	var result = 0
-	add(1, 3, &result)
-	fmt.Println("Result is ", result)
+	intToString()
+	intToFloat()
+	stringToInt()
+	stringToFloat64()
+	stringToFloat32()
+	floatToString()
+	floatToInt()
 }
 
-func add(num1 int, num2 int, result *int) {
-	*result = num1 + num2
+func intToString() {
+	age := 28
+	converted := strconv.Itoa(age)
+	fmt.Printf("%d, %s\n", age, converted)
+}
+
+func intToFloat() {
+	age := 28
+	ageStr := float32(age)
+	fmt.Printf("%d, %f\n", age, ageStr)
+}
+
+func stringToInt() {
+	age := "28"
+	converted, err := strconv.Atoi(age)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s, %d\n", age, converted)
+}
+
+func stringToFloat64() {
+	age := "28"
+	converted, err := strconv.ParseFloat(age, 64)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s, %f\n", age, converted)
+}
+
+func stringToFloat32() {
+	age := "28"
+	converted, err := strconv.ParseFloat(age, 32)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s, %f\n", age, converted)
+}
+
+func floatToString() {
+	amount := 23.50
+	converted := fmt.Sprintf("%.2f", amount)
+	fmt.Printf("%f, %s\n", amount, converted)
+}
+
+func floatToInt() {
+	amount := 23.50
+	converted := int(amount)
+	fmt.Printf("%f, %d\n", amount, converted)
 }
