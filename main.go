c6434128ddc8ba37a627563a821ac6c7788ac16c
Dhaval Shah <dhavalshah@gofynd.com>
2022-12-21 14:06:53 +0530

    Add gin and basic GET routes

--- main.go
+++ main.go
@@ -1,13 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
-func main() {
-	var result = 0
-	add(1, 3, &result)
-	fmt.Println("Result is ", result)
+type Post struct {
+	UserId int    `json:"userId"`
+	Id     int    `json:"id"`
+	Title  string `json:"title"`
+	Body   string `json:"body"`
 }
 
-func add(num1 int, num2 int, result *int) {
-	*result = num1 + num2
+var posts = []Post{
+	{Id: 1, UserId: 1, Title: "First post", Body: "First post body"},
+	{Id: 2, UserId: 1, Title: "Second post", Body: "Second post body"},
+	{Id: 3, UserId: 1, Title: "Third post", Body: "Third post body"},
+	{Id: 4, UserId: 1, Title: "Fourth post", Body: "Fourth post body"},
+}
+
+func homeRoute(context *gin.Context) {
+	context.String(http.StatusOK, "Welcome to Go Modular")
+}
+
+func postsRoute(context *gin.Context) {
+	context.JSON(http.StatusOK, posts)
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", homeRoute)
+	router.GET("/posts", postsRoute)
+
+	err := router.Run(":3000")
+	if err != nil {
+		panic(err)
+	}
 }
